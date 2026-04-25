fccb415b43b21fa4bd190f7c8e8b848386cc0ccf
Elyzaveta Perekhrestenko <ipz241_pyei@student.ztu.edu.ua>
2026-04-25 17:43:21 +0300

    Add custom FutureDate validation and HTML min attribute

---
+++ PersonalOrganizer/Models/FutureDateAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalOrganizer.Models
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is DateTime date)
+            {
+            
+                if (date.Date < DateTime.Now.Date)
+                {
+                    return new ValidationResult(ErrorMessage ?? "Дата не може бути в минулому.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
